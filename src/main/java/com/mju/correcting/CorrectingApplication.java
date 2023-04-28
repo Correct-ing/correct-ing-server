1292081ff47560eebb6a72840ef0f2581ca42a63
JinbeomKim <kst20026@gmail.com>
2023-04-29 02:33:02 +0900

    init

---
+++ src/main/java/com/mju/correcting/CorrectingApplication.java
@@ -0,0 +1,13 @@
+package com.mju.correcting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorrectingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorrectingApplication.class, args);
+	}
+
+}
