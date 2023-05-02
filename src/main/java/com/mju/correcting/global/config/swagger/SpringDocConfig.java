3d9f44742fa014e45b215e31e4da0b3bfe6d031b
JinbeomKim <kst20026@gmail.com>
2023-05-02 19:06:01 +0900

    fix: 스웨거 base url 추가

--- src/main/java/com/mju/correcting/global/config/swagger/SpringDocConfig.java
+++ src/main/java/com/mju/correcting/global/config/swagger/SpringDocConfig.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -30,6 +31,7 @@ public class SpringDocConfig {
     @Bean
     public OpenAPI springShopOpenAPI() {
         return new OpenAPI()
+                .addServersItem(new Server().url("http://correcting-env.eba-harr53pi.ap-northeast-2.elasticbeanstalk.com"))
                 .info(new Info().title("Correct-ing API")
                         .description("Correct-ing application")
                         .version("v0.0.1")
