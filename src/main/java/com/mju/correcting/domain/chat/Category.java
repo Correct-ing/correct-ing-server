1632697f265bc634739df93c9da345a14d455aad
JinbeomKim <kst20026@gmail.com>
2023-05-15 23:10:40 +0900

    fix: 채팅 응답 개선

---
+++ src/main/java/com/mju/correcting/domain/chat/Category.java
@@ -0,0 +1,5 @@
+package com.mju.correcting.domain.chat;
+
+public enum Category {
+    품사, 어순, 형식, 시제, 화법, 접속법, 부정문, 불규칙_활용, 구식문법, 정답
+}
--- src/main/java/com/mju/correcting/domain/chat/service/OpenAiService.java
+++ src/main/java/com/mju/correcting/domain/chat/service/OpenAiService.java
@@ -1,5 +1,6 @@
 package com.mju.correcting.domain.chat.service;
 
+import com.mju.correcting.domain.chat.Category;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.http.*;
@@ -9,6 +10,7 @@ import org.springframework.web.client.RestTemplate;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @Service
@@ -21,6 +23,9 @@ public class OpenAiService {
 
         // 프롬프트 설정 + 이전 대화 상황
         List<String> previousMessages = new ArrayList<>();
+
+            previousMessages.add("우리는 특정상황에 맞춰서 영어 대화를 하고 있는 중이야. 틀린 경우, 답변 앞에 []안에 [품사, 어순, 형식, 시제, 화법, 접속법, 부정문, 불규칙 활용, 구식문법] 이 9가지중 어디인지 적어주면 돼. 그리고 수정해줘. 예를 들어, [어순] I would like to order a hamburger. 맞았을 경우, [정답] 이라고 적어주고 답변 해줘!");
+        previousMessages.add("우리는 패스트 푸드에서 대화중이야");
 
         // 영어 필터링
         if (!isEnglish(prompt)) {
@@ -55,6 +60,22 @@ public class OpenAiService {
             JSONObject firstChoice = choices.getJSONObject(0);
             JSONObject message = firstChoice.getJSONObject("message");
             String content = message.getString("content");
+
+            // 결과값 처리
+            Pattern pattern = Pattern.compile("\\[(.*?)\\]");
+            Matcher matcher = pattern.matcher(content);
+
+            while (matcher.find()) {
+                String found = matcher.group(1);
+                try {
+                    Category category = Category.valueOf(found);
+                    //todo: 카테고리별 처리
+                } catch (IllegalArgumentException e) {
+                    //todo: 예외처리
+                }
+            }
+
+            // chat_log에 저장
             return content;
         } else {
             throw new RuntimeException("Failed to call OpenAI API: " + response.getStatusCode());
